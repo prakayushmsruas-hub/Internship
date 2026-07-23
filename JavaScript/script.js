4dcc8964aee3fb7b10fa5834b136864482625e2f
prakayushmsruas-hub <prakayush.msruas@gmail.com>
2026-07-23 09:24:53 +0530

    update 12

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+function greet(name){
+    console.log("Hello",name)
+}
+greet("Prakayush")
+
+let add=function(a,b){
+    return a+b
+}
+console.log(add(5,9))
+
+//Arrow funtion
+
+let multiply = (a,b) => a*b
+console.log(multiply(2,4))
